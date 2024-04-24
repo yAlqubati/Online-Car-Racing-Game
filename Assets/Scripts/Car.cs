90a1c526923893757895cf4b605591b9baee8ebd
Hasan Bera Kabadayı <hasanbera0619@gmail.com>
2024-04-25 00:08:33 +0300

    testing2

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Car : MonoBehaviour
 {
+    //comment hasso
     public float speed = 10f;
     public float increaseSpeed = 0.1f;
     public int turnDirection;
