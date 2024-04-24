0905d1b8b3208723d4eeb3122635bca60721e45b
Yousef Saif <y.alqubati56@gmail.com>
2024-04-24 22:56:13 +0300

    Initial commit

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public float speed = 10f;
+    public float increaseSpeed = 0.1f;
+    public int turnDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed += increaseSpeed * Time.deltaTime;
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(0f,turnDirection * Time.deltaTime,0f);
+        
+    }
+
+    public void turn(int direction)
+    {
+        Debug.Log("Turning to " + direction + " direction.");
+        turnDirection = direction;
+    }
+}
