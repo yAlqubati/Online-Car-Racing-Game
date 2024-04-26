1ba73d0fc0640a9c0525d13d07596d9d77a063fd
Yousef Saif <y.alqubati56@gmail.com>
2024-04-26 16:03:40 +0300

    f

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -3,18 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
+<<<<<<< HEAD
 public class Car : MonoBehaviourPun
+=======
+public class Car : MonoBehaviourPun, IPunObservable
+>>>>>>> parent of c8d1985 (added cars)
 {
     public float speed = 10f;
     public float increaseSpeed = 0.1f;
     private int turnDirection = 0;
+<<<<<<< HEAD
     PhotonView photonView;
+=======
+>>>>>>> parent of c8d1985 (added cars)
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< HEAD
         photonView = GetComponent<PhotonView>();
         
+=======
+        if (!photonView.IsMine)
+        {
+            enabled = false; // Disable script if it's not owned by the local player
+        }
+>>>>>>> parent of c8d1985 (added cars)
     }
 
     // Update is called once per frame
@@ -22,6 +36,7 @@ public class Car : MonoBehaviourPun
     {
         if (photonView.IsMine)
         {
+<<<<<<< HEAD
             speed += increaseSpeed * Time.deltaTime;
             transform.Translate(Vector3.forward * Time.deltaTime * speed);
             transform.Rotate(0f, turnDirection * Time.deltaTime, 0f);
@@ -43,6 +58,63 @@ public class Car : MonoBehaviourPun
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Obstacle") && photonView.IsMine)
+=======
+            HandleInput();
+            Move();
+        }
+        else
+        {
+            SmoothMove();
+        }
+    }
+
+    // Handle input for turning
+    private void HandleInput()
+    {
+        turnDirection = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+    }
+
+    // Move the car locally
+    private void Move()
+    {
+        speed += increaseSpeed * Time.deltaTime;
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(0f, turnDirection * Time.deltaTime, 0f);
+    }
+
+    // Smoothly move the car for remote players
+    private void SmoothMove()
+    {
+        transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * 10);
+        transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * 10);
+    }
+
+    // Synchronize position, rotation, and speed across the network
+    void IPunObservable.OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+            stream.SendNext(speed);
+        }
+        else
+        {
+            networkPosition = (Vector3)stream.ReceiveNext();
+            networkRotation = (Quaternion)stream.ReceiveNext();
+            speed = (float)stream.ReceiveNext();
+        }
+    }
+
+    // Network position and rotation variables for remote players
+    private Vector3 networkPosition;
+    private Quaternion networkRotation;
+
+    // On trigger enter
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+>>>>>>> parent of c8d1985 (added cars)
         {
             // Handle obstacle collision only for the local player
             Debug.Log("Car hit an obstacle");
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -6,20 +6,10 @@ using Photon.Pun;
 public class SpawnPlayers : MonoBehaviour
 {
     public GameObject playerPrefab;
-    public GameObject SecondPlayerPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        // if it is the master client
-        if(PhotonNetwork.IsMasterClient)
-        {
-            // spawn the player prefab
-            PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0, 0, 0), Quaternion.identity);
-        }
-        else
-        {
-            // spawn the second player prefab
-            PhotonNetwork.Instantiate(SecondPlayerPrefab.name, new Vector3(0, 0, 0), Quaternion.identity);
-        }
+        Vector3 pos = new Vector3(Random.Range(-5, 5), 0, Random.Range(-5, 5));
+        PhotonNetwork.Instantiate(playerPrefab.name, pos, Quaternion.identity);
     }
 }
