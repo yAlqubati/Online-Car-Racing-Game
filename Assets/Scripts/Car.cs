d00dad698b6fc11c2e9a263e34122f7351379599
Yousef Saif <y.alqubati56@gmail.com>
2024-04-26 15:19:49 +0300

    Update Car.cs

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -3,23 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
+
 public class Car : MonoBehaviourPun
 {
     public float speed = 10f;
     public float increaseSpeed = 0.1f;
     private int turnDirection = 0;
-    public photonView photonView;
+     PhotonView photonView;
 
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
-        if (photonView.IsMine)
-        {
-            // Enable input control only for the local player
-            // You might need to adjust this based on your input system
-            EnableInputControl();
-        }
+        
     }
     
 
@@ -34,26 +30,7 @@ public class Car : MonoBehaviourPun
         }
     }
 
-    private void EnableInputControl()
-    {
-        // Enable input control for the local player
-        // You might need to replace this with your input handling logic
-        // For example, if you are using Unity's Input system:
-        // Input.GetAxis("Horizontal") returns -1 for left and 1 for right
-        float horizontalInput = Input.GetAxis("Horizontal");
-        if (horizontalInput < 0)
-        {
-            Turn(-100);
-        }
-        else if (horizontalInput > 0)
-        {
-            Turn(100);
-        }
-        else
-        {
-            Turn(0);
-        }
-    }
+    
 
     public void Turn(int direction)
     {
@@ -61,20 +38,10 @@ public class Car : MonoBehaviourPun
         {
             // Update turn direction locally
             turnDirection = direction;
-            // Synchronize turn direction across the network
-            photonView.RPC("RPC_SetTurnDirection", RpcTarget.Others, direction);
         }
     }
 
-    [PunRPC]
-    private void RPC_SetTurnDirection(int direction)
-    {
-        if (!photonView.IsMine)
-        {
-            // Update turn direction for remote players
-            turnDirection = direction;
-        }
-    }
+    
 
     // OnTriggerEnter is called when the Collider other enters the trigger
     private void OnTriggerEnter(Collider other)
